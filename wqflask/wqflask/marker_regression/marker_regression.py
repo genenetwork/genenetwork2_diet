fb79988ed2b65c8674500383b245b932d0017471
pjotrp <pjotr.public01@thebird.nl>
2016-02-22 16:37:40 +0000

    Introducing cached values  PYLMM_COMMAND, GEMMA_COMMAND, PLINK_COMMAND and TEMPDIR

--- wqflask/wqflask/marker_regression/marker_regression.py
+++ wqflask/wqflask/marker_regression/marker_regression.py
@@ -39,16 +39,7 @@ from utility import helper_functions
 from utility import Plot, Bunch
 from utility import temp_data
 from utility.benchmark import Bench
-from utility.tools import pylmm_command, plink_command, gemma_command
-
-# Check for valid binary paths of pylmm, plink, rqtl etc. This code
-# runs at startup, so a missing binary will balk before running the
-# service
-
-GEMMA_RUN = gemma_command()
-PYLMM_RUN = pylmm_command()
-PLINK_RUN = plink_command()
-# RQTL_PATH,RQTL_COMMAND   = rqtl_command()
+from utility.tools import PYLMM_COMMAND, GEMMA_COMMAND, PLINK_COMMAND
 
 class MarkerRegression(object):
 
